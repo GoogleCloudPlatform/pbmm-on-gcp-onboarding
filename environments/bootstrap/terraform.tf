5345dd7ab713d311dcf16c512e1ee683b0d486ef
Michael O'Brien <michael@obrienlabs.org>
2023-01-30 10:21:05 -0500

    #224 - workaround TF 1.3.7 from 1.0.10

--- environments/bootstrap/terraform.tf
+++ environments/bootstrap/terraform.tf
@@ -1,5 +1,5 @@
 /**
- * Copyright 2022 Google LLC
+ * Copyright 2023 Google LLC
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,5 +16,6 @@
 
 
 terraform {
-  experiments = [module_variable_optional_attrs]
+  # 224 - experimental optional removed in TF 1.3.7 - stick with 1.0.10 in cloud build 
+  #experiments = [module_variable_optional_attrs]
 }
