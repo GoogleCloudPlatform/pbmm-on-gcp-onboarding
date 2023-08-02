d43b91d5b43a8ad29c2886d0d5ea585450494518
Michael O'Brien <michael@obrienlabs.org>
2023-08-02 14:01:28 -0400

    #271 - Update iam-groups.tf

--- environments/common/iam-groups.tf
+++ environments/common/iam-groups.tf
@@ -15,11 +15,173 @@
  */
 
 
+
 # https://github.com/GoogleCloudPlatform/pbmm-on-gcp-onboarding/issues/271
 # the terraform service account must be added in admin.google.com as a groups admin
 # https://admin.google.com/ac/roles/58150197607268354
 # move to common
-module "group" {
+module "group_opsadmin" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_opsadmin.id
+  display_name = var.iam-group_opsadmin.display_name
+  description  = var.iam-group_opsadmin.description
+  domain       = var.iam-group_opsadmin.domain
+  #owners       = var.iam-group_opsadmin.owners
+  #managers     = var.iam-group_opsadmin.managers
+  members      = var.iam-group_opsadmin.members
+}
+
+module "group_secadmin" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_secadmin.id
+  display_name = var.iam-group_secadmin.display_name
+  description  = var.iam-group_secadmin.description
+  domain       = var.iam-group_secadmin.domain
+  #owners       = var.iam-group_secadmin.owners
+  #managers     = var.iam-group_secadmin.managers
+  members      = var.iam-group_secadmin.members
+}
+module "group_telcoadmin" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_telcoadmin.id
+  display_name = var.iam-group_telcoadmin.display_name
+  description  = var.iam-group_telcoadmin.description
+  domain       = var.iam-group_telcoadmin.domain
+  #owners       = var.iam-group_telcoadmin.owners
+  #managers     = var.iam-group_telcoadmin.managers
+  members      = var.iam-group_telcoadmin.members
+}
+module "group_read" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_read.id
+  display_name = var.iam-group_read.display_name
+  description  = var.iam-group_read.description
+  domain       = var.iam-group_read.domain
+  #owners       = var.iam-group_read.owners
+  #managers     = var.iam-group_read.managers
+  members      = var.iam-group_read.members
+}
+module "group_billing" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_billing.id
+  display_name = var.iam-group_billing.display_name
+  description  = var.iam-group_billing.description
+  domain       = var.iam-group_billing.domain
+  #owners       = var.iam-group_billing.owners
+  #managers     = var.iam-group_billing.managers
+  members      = var.iam-group_billing.members
+}
+module "group_networkadmin" {
+  source  = "terraform-google-modules/group/google"
+  version = "~> 0.6"
+
+  id           = var.iam-group_networkadmin.id
+  display_name = var.iam-group_networkadmin.display_name
+  description  = var.iam-group_networkadmin.description
+  domain       = var.iam-group_networkadmin.domain
+  #owners       = var.iam-group_networkadmin.owners
+  #managers     = var.iam-group_networkadmin.managers
+  members      = var.iam-group_networkadmin.members
+}
+module "iam-groups-roles" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_opsadmin
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_opsadmin#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+
+module "iam-groups-role_networkadmin" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_networkadmin
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_networkadmin#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+
+module "iam-groups-role_secadmin" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_secadmin
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_secadmin#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+
+module "iam-groups-role_telcoadmin" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_telcoadmin
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_telcoadmin#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+
+module "iam-groups-role_read" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_read
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_read#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+
+module "iam-groups-role_billing" {
+  source           = "../../modules/iam"
+  #sa_create_assign = var.iam-groups.id #var.service_accounts
+  #project_iam      = local.project_iam
+  #folder_iam       = local.folder_iam
+  organization_iam = var.organization_iam_group_billing
+  organization     = local.organization_config.org_id
+  depends_on = [
+    module.group_billing#,
+    #module.core-org-custom-roles,
+    #module.core-folders
+  ]
+}
+/*
+# https://github.com/GoogleCloudPlatform/pbmm-on-gcp-onboarding/issues/271
+# the terraform service account must be added in admin.google.com as a groups admin
+# https://admin.google.com/ac/roles/58150197607268354
+# move to common
+module "groups" {
   source  = "terraform-google-modules/group/google"
   version = "~> 0.6"
 
@@ -40,9 +202,41 @@ module "iam-groups-roles" {
   organization_iam = var.organization_iam_groups
   organization     = local.organization_config.org_id
   depends_on = [
-    module.group#,
+    module.groups#,
     #module.core-org-custom-roles,
     #module.core-folders
   ]
-}
+}*/
 
+
+# add roles to groups
+# https://registry.terraform.io/modules/terraform-google-modules/iam/google/latest
+# https://registry.terraform.io/modules/terraform-google-modules/iam/google/7.6.0/submodules/organizations_iam
+/*module "organization-iam-bindings" {
+  source        = "terraform-google-modules/iam/google//modules/organizations_iam"
+  organizations = ["my-organization_one"]
+  #mode          = "authoritative"
+  mode          = "additive"
+
+  bindings = {
+    "roles/resourcemanager.organizationViewer" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+      "user:my-user@my-org.com",
+    ]
+    "roles/resourcemanager.projectDeleter" = [
+      "serviceAccount:my-sa@my-project.iam.gserviceaccount.com",
+      "group:my-group@my-org.com",
+      "user:my-user@my-org.com",
+    ]
+  }
+  conditional_bindings = [
+    {
+      role = "roles/editor"
+      title = "expires_after_2019_12_31"
+      description = "Expiring at midnight of 2019-12-31"
+      expression = "request.time < timestamp(\"2020-01-01T00:00:00Z\")"
+      members = ["user:my-user@my-org.com"]
+    }
+  ]
+}*/
