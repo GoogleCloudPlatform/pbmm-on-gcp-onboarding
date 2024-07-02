d07cd610568ab8a764a487641deb1cec739e7afd
rchoudhari <rchoudhari@google.com>
2024-07-02 16:55:29 +0530

    changed version 15.0 to 14.0

--- 2-environments/modules/env_baseline/monitoring.tf
+++ 2-environments/modules/env_baseline/monitoring.tf
@@ -20,7 +20,7 @@
 
 module "monitoring_project" {
   source  = "terraform-google-modules/project-factory/google"
-  version = "~> 15.0"
+  version = "~> 14.0"
 
   random_project_id           = true
   random_project_id_length    = 4
