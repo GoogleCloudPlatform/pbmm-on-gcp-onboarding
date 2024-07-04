ad53181baad15436baa5a68f6cbf7d79d9e4016d
ronakagrawal <ronakagrawal@google.com>
2024-07-04 10:51:34 +0000

    Updated remote.tf

--- 6-org-policies/dev/remote.tf
+++ 6-org-policies/dev/remote.tf
@@ -46,7 +46,7 @@ data "terraform_remote_state" "org" {
   backend = "gcs"
 
   config = {
-    bucket = "bkt-prj-b-seed-tfstate-4dc6" #var.remote_state_bucket
+    bucket = var.remote_state_bucket
     prefix = "terraform/org/state"
   }
 }
@@ -55,7 +55,7 @@ data "terraform_remote_state" "environments" {
   backend = "gcs"
 
   config = {
-    bucket = "bkt-prj-b-seed-tfstate-4dc6" #var.remote_state_bucket
+    bucket = var.remote_state_bucket
     prefix = "terraform/environments/development"
   }
 }
