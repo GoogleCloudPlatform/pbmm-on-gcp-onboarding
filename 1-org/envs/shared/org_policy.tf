354043214b7399dca27e117e1d9bea02a3a840b8
ronakagrawal <ronakagrawal@google.com>
2024-07-08 15:37:33 +0000

    Updated org_policy.tf

--- 1-org/envs/shared/org_policy.tf
+++ 1-org/envs/shared/org_policy.tf
@@ -57,9 +57,6 @@ module "organization_policies_type_boolean" {
   constraint      = "constraints/${each.value}"
 }
 
-//--->Ronak Begin
-
-//<-- Ronak End
 
 /******************************************
   Compute org policies
@@ -140,18 +137,18 @@ module "org_policies_disable_guest_attribute_access" {
   constraint  = "constraints/compute.disableGuestAttributesAccess"
 }
 
-// # /******************************************
-// #   Restrict Vpc Peering
-// # *******************************************/
-// module "org_policies_restrict_vpc_peering" {
-//   source      = "terraform-google-modules/org-policy/google"
-//   version     = "~> 5.1"
-//   enforce     = true
-//   policy_for  = "folder"
-//   folder_id   = local.folder_id
-//   policy_type = "list"
-//   constraint  = "constraints/compute.restrictVpcPeering"
-// }
+# # /******************************************
+# #   Restrict Vpc Peering
+# # *******************************************/
+# module "org_policies_restrict_vpc_peering" {
+#   source      = "terraform-google-modules/org-policy/google"
+#   version     = "~> 5.1"
+#   enforce     = true
+#   policy_for  = "folder"
+#   folder_id   = local.folder_id
+#   policy_type = "list"
+#   constraint  = "constraints/compute.restrictVpcPeering"
+# }
 
 # /******************************************
 #   IAM
