bfc4c6fe2caebc1af0857e76d78796e03e1e8fa9
rchoudhari <rchoudhari@google.com>
2024-07-09 13:49:23 +0530

    Added script for 6-orgpolices / whole

--- automation-scripts/6-org-policies/6-org-policies.sh
+++ automation-scripts/6-org-policies/6-org-policies.sh
@@ -1,6 +1,9 @@
 
 cd $base_dir/6-org-policies
 
+cp ../build/tf-wrapper.sh .
+chmod 755 ./tf-wrapper.sh
+
 #get organization_id
 export ORGANIZATION_ID=$(terraform -chdir="../0-bootstrap/" output -json common_config | jq '.org_id' --raw-output)
 gcloud scc notifications describe "scc-notify" --organization=${ORGANIZATION_ID}
@@ -12,9 +15,14 @@ echo ${GOOGLE_IMPERSONATE_SERVICE_ACCOUNT}
 export backend_bucket=$(terraform -chdir="../0-bootstrap/" output -raw gcs_bucket_tfstate)
 echo "remote_state_bucket = ${backend_bucket}"
 
-sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./common.auto.tfvars
+# sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./common.auto.tfvars
+sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./common/terraform.tfvars
+sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./development/terraform.tfvars
+sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./nonproduction/terraform.tfvars
+sed -i'' -e "s/REMOTE_STATE_BUCKET/${backend_bucket}/" ./production/terraform.tfvars
+# cat ./common.auto.tfvars
 
-cat ./common.auto.tfvars
+cd ./common 
 
 terraform init
 
@@ -27,6 +35,23 @@ terraform plan -input=false -out org_policy.tfplan
 
 #-var="gcp_credentials_file=$GOOGLE_APPLICATION_CREDENTIALS"
 terraform apply org_policy.tfplan
+
+cd ..
+
+./tf-wrapper.sh init development
+./tf-wrapper.sh plan development
+./tf-wrapper.sh validate development $(pwd)/../policy-library ${CLOUD_BUILD_PROJECT_ID}
+./tf-wrapper.sh apply development
+
+./tf-wrapper.sh init nonproduction
+./tf-wrapper.sh plan nonproduction
+./tf-wrapper.sh validate nonproduction $(pwd)/../policy-library ${CLOUD_BUILD_PROJECT_ID}
+./tf-wrapper.sh apply nonproduction
+
+./tf-wrapper.sh init production
+./tf-wrapper.sh plan production
+./tf-wrapper.sh validate production $(pwd)/../policy-library ${CLOUD_BUILD_PROJECT_ID}
+./tf-wrapper.sh apply production
 
 unset GOOGLE_IMPERSONATE_SERVICE_ACCOUNT
 
