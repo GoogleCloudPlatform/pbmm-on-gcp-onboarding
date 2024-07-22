10799ccd6802241ba8fc95b99046ebd69a173897
rchoudhari <rchoudhari@google.com>
2024-07-22 17:46:18 +0530

    Added new fortigate bucket

--- 0-bootstrap/fortigate_image_bucket.tf
+++ 0-bootstrap/fortigate_image_bucket.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "fortigate_image_bucket" {
   name          = local.name
   location      = var.default_region
   force_destroy = true
+  project       = module.seed_bootstrap.seed_project_id
 
   uniform_bucket_level_access = true
 }
