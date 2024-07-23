cc7026689c98f9f256c024f28e64afa35eb5f057
ronakagrawal <ronakagrawal@google.com>
2024-07-23 09:10:33 +0000

    Updated variables.tf

--- 0-bootstrap/variables.tf
+++ 0-bootstrap/variables.tf
@@ -349,11 +349,6 @@ variable "initial_group_config" {
 #   description = "If false TFC will provide remote runners to run the jobs. If true, TFC will use Agents on a private autopilot GKE cluster."
 #   default = false
 # }
-variable "gcp_credentials_file" {
-  description = "Path to the Google Cloud Platform service account key file"
-  type        = string
-  default = "none"
-}
 
 variable "nictype" {
   type    = string
