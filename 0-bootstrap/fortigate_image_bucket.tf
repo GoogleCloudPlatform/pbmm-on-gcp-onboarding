e2d50904c49e88f1eee1ad010859e9940652f889
rchoudhari <rchoudhari@google.com>
2024-07-23 22:34:04 +0530

    Testing Parametization of Network creation using variables,comment fortigate bucket

--- 0-bootstrap/fortigate_image_bucket.tf
+++ 0-bootstrap/fortigate_image_bucket.tf
@@ -1,12 +1,12 @@
-resource "google_storage_bucket" "fortigate_image_bucket" {
-  name          = local.name
-  location      = var.default_region
-  force_destroy = true
-  project       = module.seed_bootstrap.seed_project_id
+# resource "google_storage_bucket" "fortigate_image_bucket" {
+#   name          = "${local.name}-${var.random_suffix}"
+#   location      = var.default_region
+#   force_destroy = true
+#   project       = module.seed_bootstrap.seed_project_id
 
-  uniform_bucket_level_access = true
-}
+#   uniform_bucket_level_access = true
+# }
 
-locals {
-    name = "fortigate_image_bucket"
-}
+# locals {
+#     name = "fortigate_image_bucket"
+# }
