8519b42a9f946b63ec721b05c74d46d2f2fbeed9
rchoudhari <rchoudhari@google.com>
2024-07-24 15:50:01 +0530

    Networks spoke sed command testing

--- 0-bootstrap/variables.tf
+++ 0-bootstrap/variables.tf
@@ -349,8 +349,5 @@ variable "initial_group_config" {
 #   description = "If false TFC will provide remote runners to run the jobs. If true, TFC will use Agents on a private autopilot GKE cluster."
 #   default = false
 # }
-variable "random_suffix" {
-  description = "Appends a 6 character random suffix to project ID "
-  type        = bool
-}
+
 
